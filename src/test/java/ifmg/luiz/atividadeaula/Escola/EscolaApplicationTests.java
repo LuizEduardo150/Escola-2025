658e63ace176ff8df723020398475358ab098e88
LuizEduardo150 <luizedluimf10@gmail.com>
2025-06-24 14:01:20 -0300

    criacao projeto

---
+++ src/test/java/ifmg/luiz/atividadeaula/Escola/EscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package ifmg.luiz.atividadeaula.Escola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
